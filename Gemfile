cb1dbeb0c18dd24d7dcd41fd00763b4e9473c384
Sam Deacon <deaconsam@hotmail.co.uk>
2015-07-29 18:17:37 +0100

    Added umage/paperclip uploading ** plus temporary initializer **

--- Gemfile
+++ Gemfile
@@ -11,8 +11,10 @@ gem 'turbolinks'
 gem 'bootstrap-sass', '~> 3.3.5.1'
 gem 'simple_form', '~> 3.1.0'
 gem 'devise', '~> 3.5.1'
+gem 'paperclip', "~> 4.2"
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'spring',        group: :development
 
 
 
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
   before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
   def index
     @pins = Pin.all.order("created_at DESC")
   end
@@ -40,11 +41,11 @@ class PinsController < ApplicationController
 
   private
 
-  def pin_params
-    params.require(:pin).permit(:title, :description)
-  end
+	def pin_params
+		params.require(:pin).permit(:title, :description, :image)
+	end
 
-  def find_pin
-    @pin = Pin.find(params[:id])
-  end
+	def find_pin
+		@pin = Pin.find(params[:id])
+	end
 end
